c898e492abd86fee77960b50f12bf9b4d59591aa
Nan Zou <nanzou_leeds@hotmail.com>
2020-05-12 01:56:26 +0800

    canny

--- mission_detect.py
+++ mission_detect.py
@@ -26,7 +26,7 @@ cv2.rectangle(img, (0,max_loc[1]-50), (img.shape[1], max_loc[1] + 300), 0, 2)
 plt.imshow(img)
 '''
 
-mission_img = img[(max_loc[1]-50):(max_loc[1] + 300),0:(max_loc[1])-50,] # Y, X,RGB
+mission_img = img[(max_loc[1]-50):(max_loc[1] + 300),0:(max_loc[1])-50,].copy() # Y, X,RGB
 plt.imshow(mission_img)
 #mission_hsv = cv2.cvtColor(mission_img,cv2.COLOR_BGR2HSV)
 missiion_img_copy = mission_img.copy()
@@ -53,31 +53,25 @@ cv2.drawContours(missiion_img_copy, contours, -1, (0, 255,0), 1)
 plt.imshow(missiion_img_copy)
 
 
-lower = [40,30,10]
-upper_2 = [48,36,17]
+lower = [35,30,10]
+upper_2 = [50,36,17]
 lower = np.array(lower, dtype="uint8")  # 颜色下限
 upper_2 = np.array(upper_2, dtype="uint8")  # 颜色上限
 # 根据阈值找到对应颜色
 mask_2 = cv2.inRange(output, lower, upper_2)    #查找处于范围区间的
-mask_2 = 255-mask_2                          #留下铝材区域
+#mask_2 = 255-mask_2                          #留下铝材区域
 output_2 = cv2.bitwise_and(output, output, mask=mask_2)    #获取铝材区域
 #bgroutput = cv2.cvtColor(output,cv2.COLOR_HSV2BGR)
 # 展示图片
 plt.imshow(np.hstack([mission_img, output_2]))
 
-
-contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #RETR_TREE
-print(mask.shape)
-#print(mask[0])
-#print(len(contours))
-cv2.drawContours(mission_img, contours, -1, (0, 255,0), 1)
-plt.imshow(mission_img)
-## draw outline for resources
+edged = cv2.Canny(missiion_img_copy, 10, 5)
+plt.imshow(edged)
+#https://blog.csdn.net/m0_37857300/article/details/84973197?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.nonecase
 
 
 
-
-text = pytesseract.image_to_string(output)
+text = pytesseract.image_to_string(missiion_img_copy)
 print(text)
 
 
